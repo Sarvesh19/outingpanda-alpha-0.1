c54aee31703c55b08e575995e274409ed46f4f4c
SARVESH YADAV <sarveshyadav@SARVESHs-MacBook-Air.local>
2020-04-04 20:34:35 +0530

    first commit

---
+++ src/app/student.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StudentService {
+
+  private baseUrl = 'http://localhost:8083/employees/';
+
+  constructor(private http:HttpClient) { }
+
+  getStudentList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`+'getAllEmpolyee');
+  }
+
+  createStudent(student: object): Observable<object> {
+    return this.http.post(`${this.baseUrl}`+'save-employee', student);
+  }
+
+  deleteStudent(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl} + 'delete-student'/${id}`, { responseType: 'text' });
+  }
+
+  getStudent(id: number): Observable<Object> {
+    return this.http.get(`${this.baseUrl}/student/${id}`);
+  }
+
+  updateStudent(id: number, value: any): Observable<Object> {
+    return this.http.post(`${this.baseUrl}/update-student/${id}`, value);
+  }
+  
+}                                           
---
+++ src/app/student.ts
@@ -0,0 +1,7 @@
+export class Student {
+
+    id:number;
+    firstName:String;
+    email:String;
+    lastName :String;
+}
